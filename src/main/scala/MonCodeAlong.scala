ccbc91e01f9b805da305b6a33720f3fcd6e0e45c
Andrew Boyce <andrew.boyce@MacBookPro-2.powerhub>
2025-05-19 15:15:21 +0100

    complete task 1-4

--- src/main/scala/MonCodeAlong.scala
+++ src/main/scala/MonCodeAlong.scala
@@ -55,29 +55,88 @@ object MonCodeAlong extends App {
   val not: Boolean = !false
 
   //METHODS
-  def makeACupOfTea(sugar:Int, milk:Boolean): String = {
+  def makeACupOfTea(sugar: Int, milk: Boolean): String = {
     s"You have made a cup of tea with $sugar spoons of sugar. Your milk selection is: $milk"
   }
+
   println(makeACupOfTea(sugar = 7, milk = true)) //calling the method
 
   val vat: Double = 1.2
   //Write a method that calculates the final price of a product with VAT
 
-  def priceWithVAT(price:Double): Double = {
+  def priceWithVAT(price: Double): Double = {
     price * vat
   }
+
   println(priceWithVAT(price = 10.0))
 
   //CLASSES think of these as a type
-  class Dog (val name:String, val age:Int, val likesBananas:Boolean) {
+  class Dog(val name: String, val age: Int, val likesBananas: Boolean) {
     def speak: String = "woof" //declared in advance as it is the same for every Dog this could be
     // overridden if you wanted
   }
+
   //Use the class to make objects (e.g Dog)
-  val loki:Dog = new Dog("Loki", 5, true)
+  val loki: Dog = new Dog("Loki", 5, true)
   println(loki.speak)
   println(loki.name)
 
+  //AFTERNOON TASK
+  //TASK 1
+  //a) false
+  val aa: Boolean = (3 + 4) * 57 < 300
+  print("a) ")
+  println(aa)
+  //b) true
+  val bb: Boolean = (144 / 12) >= 12
+  print("b) ")
+  println(bb)
+  //c) false WRONG actually true
+  val cc: Boolean = "Cat" < "Dog"
+  print("c) ")
+  println(cc)
+  //I was wrong because it doesn't compare the length of the string but the Unicode values
+  //it does this letter by letter so 'C' is 67 and 'D' is 68 so it stops there. If I was
+  //comparing 'Car' and 'Cat' it would compare 'C' and 'C', then move to 'a' and 'a' then to
+  //'r' and 't'. But it only moves on if the 2 characters in the string are the same.
+  //d) false
+  val dd: Boolean = "Rabbit" < "Hamster"
+  print("d) ")
+  println(dd)
+  //e) true
+  val ee: Boolean = (17 % 2) == 1
+  print("e) ")
+  println(ee)
+  //f) true
+  val ff: Boolean = ((75 / 9) < 30) && ((89 / 6) < 20)
+  print("f) ")
+  println(ff)
 
+  //TASK 2
+  //"I love scala already" is a string whereas println("I love scala already") is a unit.
+  //Also a string is data that is stored whereas println is an action that prints to the console.
+
+  //TASK 3
+  def squareNumber(num: Int): Int = {
+    num * num
+  }
+  println(squareNumber(3)) //9
+
+  //TASK 4
+  def isItOdd(num: Int): Boolean = {
+    (num % 2) == 1
+  }
+  println(isItOdd(3)) //true
+  println(isItOdd(8)) //false
+
+  //TASK 5
+  class Book(val title: String, val pages: Int, val genre: String, val isFiction: Boolean) {
+    val fictionStatus: String = {
+
+    }
+    def description: String = {
+      s"$title is a $"
+    }
+  }
 
 }

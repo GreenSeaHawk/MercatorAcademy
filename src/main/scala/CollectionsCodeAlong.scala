3b841107485a1a3777da6ddbd5fd048eeca2626d
Andrew Boyce <andrew.boyce@MacBookPro-2.powerhub>
2025-05-20 17:21:11 +0100

    complete afternoon tasks

--- src/main/scala/CollectionsCodeAlong.scala
+++ src/main/scala/CollectionsCodeAlong.scala
@@ -1,8 +1,8 @@
 object CollectionsCodeAlong extends App {
-  val firstSet: Set[Int] = Set(1,2,3,4,5,5) //Unordered, does not allow repeats
+  val firstSet: Set[Int] = Set(1, 2, 3, 4, 5, 5) //Unordered, does not allow repeats
   println("First set: " + firstSet)
   //Can't mix and match what's in a set, need to use a map if you want to have ints and stings
-  val firstSeq: Seq[Int] = Seq(1,2,3,4,5,5) //Ordered, allows repeats
+  val firstSeq: Seq[Int] = Seq(1, 2, 3, 4, 5, 5) //Ordered, allows repeats
   println("First sequence: " + firstSeq)
 
   //Maps take 2 params (Key -> Value)
@@ -59,7 +59,7 @@ object CollectionsCodeAlong extends App {
   val blueKeyFilter: Map[Int, String] = colourMap.filter(_._2 == "blue")
   println("filter by blue: " + blueKey)
   println("filter by blue 2: " + blueKeyFilter)
-  val setOfInts: Set[Int] = Set(1,2,3,4,5,6,7,8)
+  val setOfInts: Set[Int] = Set(1, 2, 3, 4, 5, 6, 7, 8)
 
   val intsHigherThan2: Set[Int] = setOfInts.filter(_ > 2)
   println("intsHigherThan2: " + intsHigherThan2)
@@ -68,20 +68,23 @@ object CollectionsCodeAlong extends App {
   //Use a map to iterate a collection
 
   def tripledSet(inputSet: Set[Int]): Set[Int] = inputSet.map {
-//    _ * 3
+    //    _ * 3
     num => num * 3 //both these methods mean the same
   }
+
   println("Tripled set: " + tripledSet(setOfInts))
 
   def tripledSeq(inputSeq: Seq[Int]): Seq[Int] = inputSeq.map {
     //    _ * 3
     num => num * 3 //both these methods mean the same
   }
+
   println("Tripled seq: " + tripledSeq(firstSeq))
 
   def tripledMap(inputMap: Map[String, Int]): Map[String, Int] = inputMap.map {
     case (key, value) => (key + "s", value * 3) //can just do one of these if you like
   }
+
   println("Tripled map: " + tripledMap(firstMap))
 
   // .exists = all that could be (get collection back with all that match the criteria)
@@ -92,9 +95,11 @@ object CollectionsCodeAlong extends App {
   def add1(inputSet: Set[Int]): Set[Int] = inputSet.map {
     _ + 1
   }
+
   println("Add 1 to set: " + add1(setOfInts))
 
   def containsR(inputSeq: Seq[String]): Boolean = inputSeq.exists(_.contains("r"))
+
   println("Contains r: " + containsR(seqOfNames))
   //I'm a little confused by exactly what's happened here
 
@@ -105,7 +110,7 @@ object CollectionsCodeAlong extends App {
   val flatMapNames: Seq[Char] = seqOfNames.flatMap(_.toUpperCase)
   println("FlatMap: " + flatMapNames)
 
-  val newSeq: Seq[Int] = Seq(6,7,8)
+  val newSeq: Seq[Int] = Seq(6, 7, 8)
   val appendedListOne = firstSeq :+ newSeq
   println("Appended with :+ " + appendedListOne) //appends list as a value
   val appendedListTwo = firstSeq ++ newSeq
@@ -115,5 +120,90 @@ object CollectionsCodeAlong extends App {
   println("Prepended with ++ " + prependedListOne)
   val prependedListTwo = newSeq +: firstSeq
   println("Prepended with +: " + prependedListTwo)
+
+  //AFTERNOON TASK
+  //1
+  val forenamesSeq: Seq[String] = Seq("Pablo", "Andrew", "Chay", "Shani", "Pablo", "Ajan", "Pablo", "Shani", "Camille")
+  //2
+  println(s"${forenamesSeq(0)}, ${forenamesSeq(1)} ") //found a better way below
+  println(s"${forenamesSeq.mkString(", ")}")
+  //3
+  def countTaxReturns(inputSeq: Seq[String], inputUser: String): Int = {
+    inputSeq.count(_ == inputUser)
+  }
+  //4
+  println("How many times Pablo submitted return: " + countTaxReturns(forenamesSeq, "Pablo"))
+  //5
+  val forenamesSet: Set[String] = forenamesSeq.toSet
+  //6
+  println(s"Names of unique taxpayers: ${forenamesSet.mkString(", ")}")
+  //7
+  def countTaxReturnsAll(input: Iterable[String], inputUser: String): Int = {
+    input.count(_ == inputUser)
+  }
+  //8
+  println(s"Pablo appearances in seq: ${countTaxReturnsAll(forenamesSeq, "Pablo")}")
+  println(s"Pablo appearances in set: ${countTaxReturnsAll(forenamesSet, "Pablo")}")
+  //9
+  //a
+  val userLoginsMap: Map[String, Int] = Map(
+    "Pablo" -> 2,
+    "Andrew" -> 1,
+    "Chay" -> 7,
+    "Shani" -> 4,
+    "Camille" -> 5,
+    "Ajan" -> 1
+  )
+  //b
+  println("First user has: " + userLoginsMap("Pablo") + " failed attempts")
+  //c
+  val newUserMap: Map[String, Int] = Map("Yanrong" -> 3)
+  val updatedLoginsMap = userLoginsMap ++ newUserMap
+  println(updatedLoginsMap)
+  //d
+  def addFailedAttempt(inputMap: Map[String, Int], inputString: String): Map[String, Int] = inputMap.map {
+    case (key, value) if key == inputString => (key, value + 1) //can just do one of these if you like
+    case pair => pair
+  }
+  val userLoginsMap1FAAdded: Map[String, Int] = addFailedAttempt(updatedLoginsMap, "Andrew")
+  println("Map with Andrews failed attempts +1: " + userLoginsMap1FAAdded)
+  //e
+  val loginsMapUserRemoved: Map[String, Int] = userLoginsMap1FAAdded - "Camille"
+  println("Map with Camille removed: " + loginsMapUserRemoved)
+
+  //EXTENSION
+  val day1Set: Set[String] = Set("Camille", "Andrew", "Ajan", "Pablo", "Chay", "Shani")
+  val day2Set: Set[String] = Set("Andrew", "Ajan", "Pablo", "Chay", "Yanrong", "Adam", "April")
+  //a
+  val bothDaysSet: Set[String] = day1Set.intersect(day2Set)
+  println("Submitted on both days: " + bothDaysSet)
+  //b
+  val onlyDay1Set: Set[String] = day1Set.diff(day2Set)
+  println("Only submitted on the first day " + onlyDay1Set)
+  //c
+  val uniqueSubmittersSet: Set[String] = day2Set ++ day1Set
+  val uniqueSubmittersList: List[String] = uniqueSubmittersSet.toList
+  println(uniqueSubmittersSet)
+  println("List on unique submitters: " + uniqueSubmittersList)
+
+  //RESEARCH
+  //1
+  //Seq is ordered and can contain duplicates
+  //Set is unordered and cannot contain duplicates
+  //So a set is useful when needing to know if a particular item exists and you aren't worried
+  //about duplicates. A seq is useful when you need to do something sequentially. (Maybe if you
+  //need to iterate over it?
+  //2 A)
+  //i) Map.get(key) returns Some(value) if key exists else None
+  println(userLoginsMap.get("Pablo")) //returns Some(2)
+  //ii) Map(key) returns the value if key exists else NoSuchElementException
+  println(userLoginsMap("Pablo")) //returns 2
+  //iii) Map.getOrElse(key, default) returns the value if key exists else default
+  println(userLoginsMap.getOrElse("Pablo", 0)) //returns 2
+  println(userLoginsMap.getOrElse("Adrian", 0)) //returns 0
+  //B) see above
+  //C)
+  //Could be dangerous if that key doesn't exist as it will throw an error but probably not
+  //the error we are looking for
 
 }

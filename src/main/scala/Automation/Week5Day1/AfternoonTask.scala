2d39981748bfdef86e94a16117a1b20af7e3587a
Andrew Boyce <andrew.boyce96@hotmail.com>
2025-06-09 15:57:18 +0100

    fixed browser dimensions issues

--- src/main/scala/Automation/Week5Day1/AfternoonTask.scala
+++ src/main/scala/Automation/Week5Day1/AfternoonTask.scala
@@ -1,8 +1,11 @@
 package Automation.Week5Day1
 
-import Automation.Week4Day4.FindByTask.driver
-import org.openqa.selenium.{By, Dimension, JavascriptExecutor, Point, WebDriver, WebElement}
+
+import org.openqa.selenium.{By, Dimension, Point, WebDriver, WebElement}
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.support.ui.{ExpectedCondition, WebDriverWait}
+import org.openqa.selenium.JavascriptExecutor
+import java.time.Duration
 
 object AfternoonTask extends App {
 
@@ -10,8 +13,8 @@ object AfternoonTask extends App {
   val driver: WebDriver = new ChromeDriver()
   driver.get("https://testpages.herokuapp.com/styled/basic-html-form-test.html")
 //  Set the browser to fit on the left-hand side of my monitor (this broke EvilTester)
-//  driver.manage().window().setPosition(new Point(-1920, 0))
-//  driver.manage().window().setSize(new Dimension(960, 1080))
+  driver.manage().window().setPosition(new Point(-1920, 0))
+  driver.manage().window().setSize(new Dimension(960, 1080))
 
 //  Username
   val username: WebElement = driver.findElement(By.name("username"))
@@ -35,13 +38,31 @@ object AfternoonTask extends App {
   checkbox2.click()
   println("Checkbox 2 has been selected, method: By.xpath")
 
-  Thread.sleep(2000)
+  Thread.sleep(4000)
 
-  //  Submit
+//  Scroll Down/Resize
+  val js = driver.asInstanceOf[JavascriptExecutor]
+//  js.executeScript("window.scrollTo(0, document.body.scrollHeight);")
+
+
+  Thread.sleep(1000)
+
+//  Submit
   val submitButtons: java.util.List[WebElement] = driver.findElements(By.className("styled-click-button"))
   val submit: WebElement = submitButtons.get(1)
+  js.executeScript("arguments[0].scrollIntoView();", submit)
   submit.click()
   println("Submit button has been clicked, method: By.className")
+
+//  val submit: WebElement = driver.findElement(By.cssSelector("#HTMLFormElements > table > tbody > tr:nth-child(9) > td > input:nth-child(2)"))
+//  submit.click()
+//  println("Submit button has been clicked, method: By.cssSelector")
+
+//  val myDynamicSubmit = new WebDriverWait(driver, Duration.ofSeconds(10)).until(
+//    new ExpectedCondition[WebElement] {
+//      override def apply(d: WebDriver) = d.findElement(By.cssSelector("#HTMLFormElements > table > tbody > tr:nth-child(9) > td > input:nth-child(2)"))
+//    })
+//  myDynamicSubmit.click()
 
   Thread.sleep(4000)
 
@@ -51,15 +72,18 @@ object AfternoonTask extends App {
 
   Thread.sleep(4000)
 
-//  Scroll to the bottom of the browser
-//  val js = driver.asInstanceOf[JavascriptExecutor]
-//  js.executeScript("window.scrollTo(0, document.body.scrollHeight);")
-
   val evilTester: WebElement = driver.findElement(By.linkText("EvilTester.com"))
+  js.executeScript("arguments[0].scrollIntoView();", evilTester)
   evilTester.click()
   println("EvilTester.com link clicked, method: By.linkText")
 
-  Thread.sleep(2000)
+//  val myDynamicEvilTester = new WebDriverWait(driver, Duration.ofSeconds(10)).until(
+//    new ExpectedCondition[WebElement] {
+//      override def apply(d: WebDriver) = d.findElement(By.linkText("EvilTester.com"))
+//    })
+//  myDynamicEvilTester.click()
+
+  Thread.sleep(4000)
 
   driver.quit()
 
